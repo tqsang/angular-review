2e61d27e651af659b3272b0407f766625c0038ed
tranquangsang2702 <tranquangsang2702@gmail.com>
2021-05-22 16:07:53 +0700

    checkout develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentItemComponent } from './student/student-item/student-item.component';
+import { StudentDetailComponent } from './student/student-detail/student-detail.component';
+import { StudentEditComponent } from './student/student-edit/student-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    HeaderComponent,
+    StudentListComponent,
+    StudentItemComponent,
+    StudentDetailComponent,
+    StudentEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/student.model.ts
@@ -0,0 +1,11 @@
+export class Students {
+  public id: string;
+  public name: string;
+  public dob: string;
+
+  constructor(id: string, name: string, dob: string) {
+    this.id = id;
+    this.name = name;
+    this.dob = dob;
+  }
+}
